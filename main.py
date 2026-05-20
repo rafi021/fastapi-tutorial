7b599b42b9d397318a23f9e76d677b3d50392944
Eng. Mahmud Ibrahim <mahmud.ibrahim021@gmail.com>
2026-05-21 01:19:29 +0600

    validation using pydantic

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
 
 app = FastAPI()
 
-# Products with 20 items
-products = [f"Product {i}" for i in range(1, 21)]
+# Products with name, price, stock --> add 20 items
+products = [
+    {"name": f"Product {i}", "price": i * 10.0, "stock": i * 5}
+    for i in range(1, 21)
+]
+
+class CreateProductRequest (BaseModel):
+    name: str
+    price: float
+    stock: int
+
 
 
 # Query Parameters ---> optional params (search, sort) --> limit (default value)
@@ -19,9 +28,13 @@ def get_products(search: str = None, limit: int = 10, sort: str = None):
     return {"products": filtered_products[:limit]}
 
 
-# Create new Product
+# Create new Product with pydantic validation
 @app.post("/products")
-def create_product(name: str):
-    new_product = f"Product {len(products) + 1}: {name}"
+def create_product(product: CreateProductRequest):
+    new_product = {
+        "name": product.name,
+        "price": product.price,
+        "stock": product.stock
+    }
     products.append(new_product)
     return {"message": "Product created", "product": new_product}
