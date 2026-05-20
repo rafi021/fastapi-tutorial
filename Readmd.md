bf5c3e5f1e326cb2647aeae86faec8776598fc8a
Eng. Mahmud Ibrahim <mahmud.ibrahim021@gmail.com>
2026-05-21 00:52:06 +0600

    create basic route, GET and Swagger docs

---
+++ Readmd.md
@@ -0,0 +1,7 @@
+## Run Server
+```
+uvicorn main:app --reload
+```
+
+## Swagger docs
+http://127.0.0.1:8000/docs
--- main.py
+++ main.py
@@ -6,3 +6,19 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "Hello, World!"}
+
+@app.get("/about")
+def about():
+    return {"message": "This is the about page."}
+
+@app.get("/contact")
+def contact():
+    contact = {
+        "email": "contact@example.com",
+        "phone": "01111111111111"
+    }
+    return {
+        "status": "success",
+        "message": "Contact us at contact@example.com",
+        "data": contact
+    }
