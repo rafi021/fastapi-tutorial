967589c8abb283528666c44eab83b37cbffc7a57
Eng. Mahmud Ibrahim <mahmud.ibrahim021@gmail.com>
2026-05-24 02:40:45 +0600

    File upload + Serve Static Files

--- main.py
+++ main.py
@@ -1,70 +1,46 @@
-from fastapi import FastAPI, HTTPException, Depends, Header
-from jose import jwt, JWTError
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from datetime import datetime,timezone, timedelta
-import bcrypt
+from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi.staticfiles import StaticFiles
+import os
+import shutil   # For file operations
+
 
 app = FastAPI()
 
+# Step 1: Create 'uploads' directory if it doesn't exist
+UPLOAD_DIR = "uploads"
+if not os.path.exists(UPLOAD_DIR):
+    os.makedirs(UPLOAD_DIR)
 
-#JWT Configuration
-SECRET_KEY = "mysecretkey"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-#OAuth2 Setup
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
-
-# --- Native Hashing Functions --- 
-def hash_password(password: str):
-    pwd_bytes = password.encode('utf-8')
-    salt = bcrypt.gensalt()
-    return bcrypt.hashpw(pwd_bytes, salt).decode('utf-8')
-
-# Verify Password
-def verify_password(plain_password: str, hashed_password: str)-> bool:
-    return bcrypt.checkpw(plain_password.encode('utf-8'), hashed_password.encode('utf-8'))
+# Step 2: Mount 'uploads' directory to serve static files
+# URL: http://localhost:8000/files/your_uploaded_file.ext
+app.mount("/files", StaticFiles(directory=UPLOAD_DIR), name="files")
 
 
-# Dummy User Database
-fake_users_db = {
-    "admin": {
-        "username": "admin",
-        "hashed_password": hash_password("1234"),
-    }
-}
 
-# Create Token
-def create_access_token(data: dict):
-    to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode.update({"exp": expire})
-    token = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return token
-
-
-# Verify Token
-def verify_token(token: str = Depends(oauth2_scheme)):
+# Step 3: Save the uploaded file to the 'uploads' directory
+@app.post("/upload")
+async def upload_file(file: UploadFile = File(...)):
+    filename = file.filename
+    file_Path = os.path.join(UPLOAD_DIR, filename)
+    
+    if not filename:
+        raise HTTPException(status_code=400, detail="No file uploaded.")
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise HTTPException(status_code=401, detail="Invalid or Expired token")
-        return username
-    except JWTError:
-        raise HTTPException(status_code=401, detail="Invalid or Expired token")
+        with open(file_Path, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Failed to save file: {str(e)}")
+    
+    return {
+        "message": "File uploaded successfully.",
+        "filename": filename, "url": f"/files/{filename}"
+        }
 
-# Login API (Token Generate - OAuth2)
-@app.post("/login")
-def login(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = fake_users_db.get(form_data.username)
-    if not user or not verify_password(form_data.password, user["hashed_password"]):
-        raise HTTPException(status_code=401, detail="Invalid credentials")
-    access_token = create_access_token(data={"sub": form_data.username})
-    return {"access_token": access_token, "token_type": "bearer"}
-
-
-# Protected API
-@app.get("/protected")
-def protected_route(username: str = Depends(verify_token)):
-    return {"message": f"Hello, {username}. This is a protected route!"}
+# Step 4: Get the uploaded file using the URL provided in the response of the upload endpoint.
+# Example: http://localhost:8000/files/your_uploaded_file.ext
+@app.get("/files/{filename}")
+async def get_file(filename: str):
+    file_path = os.path.join(UPLOAD_DIR, filename)
+    if not os.path.exists(file_path):
+        raise HTTPException(status_code=404, detail="File not found.")
+    return {"message": "File exists.", "url": f"http://localhost:8000/files/{filename}"}
