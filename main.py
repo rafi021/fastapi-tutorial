888339df0e2a897d3fc329b29356aa5ddce30641
Eng. Mahmud Ibrahim <mahmud.ibrahim021@gmail.com>
2026-05-24 01:56:50 +0600

    auth basic, jwt  intro

--- main.py
+++ main.py
@@ -1,104 +1,44 @@
-from fastapi import FastAPI, Request, Depends, HTTPException
-from sqlalchemy import create_engine, Column, Integer, String, Boolean, DateTime
-from sqlalchemy.orm import sessionmaker, declarative_base, Session
-from pydantic import BaseModel
-from datetime import datetime
-import asyncio
-
-# MYSQL Database setup
-DATABASE_URL = "mysql+pymysql://root:lerd@127.0.0.1/fastapi_db"
-
-engine = create_engine(DATABASE_URL, connect_args={"charset": "utf8mb4"}, echo=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+from fastapi import FastAPI, HTTPException, Depends, Header
+from jose import jwt, JWTError
+from datetime import datetime,timezone, timedelta
 
 
-# Define a Todo model
-class Todo(Base):
-    __tablename__='todos'
-
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String(100), index=True)
-    completed = Column(Boolean, default=False)
-    created_at = Column(DateTime, default=datetime.now)
-    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
-
-# Create the database tables
-Base.metadata.create_all(bind=engine, checkfirst=True)
-
-# Initialize FastAPI app
 app = FastAPI()
 
-# Dependency to get DB session
-def get_db():
-    db = SessionLocal()
+SECRET_KEY = "mysecretkey"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+# Create Token
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    token = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return token
+
+
+# Verify Token
+def verify_token(token: str = Header(None)):
     try:
-        yield db
-    finally:
-        db.close()
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise HTTPException(status_code=401, detail="Invalid or Expired token")
+        return username
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid or Expired token")
 
-# Request to create a new todo item
-class TodoCreateRequest(BaseModel):
-    title: str
-    completed: bool = False
+# Login API (Token Generate)
+@app.post("/login")
+def login(username: str, password: str):
+    if username != "admin" or password != "123456":
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+    access_token = create_access_token(data={"sub": username})
+    return {"access_token": access_token, "token_type": "bearer"}
 
-# Response model for a todo item
-class TodoResponse(BaseModel):
-    id: int
-    title: str
-    completed: bool
 
-@app.get("/")
-async def home():
-    await asyncio.sleep(3)  # Simulate a delay
-    return {"message": "Welcome to the Todo API"}
-
-@app.post("/todos/", response_model=TodoResponse)
-async def create_todo(request: TodoCreateRequest, db: Session = Depends(get_db)):
-    new_todo = Todo(**request.dict())
-    db.add(new_todo)
-    db.commit()
-    db.refresh(new_todo)
-    return new_todo
-
-@app.get("/todos", response_model=list[TodoResponse])
-async def get_todos(db: Session = Depends(get_db)):
-    todos = db.query(Todo).all()
-    return todos
-
-@app.get("/todos/{todo_id}", response_model=TodoResponse)
-async def get_todo(todo_id: int, db: Session = Depends(get_db)):
-    todo = db.query(Todo).filter(Todo.id == todo_id).first()
-    if not todo:
-        raise HTTPException(status_code=404, detail="Todo not found")
-    return todo
-
-@app.put("/todos/{todo_id}", response_model=TodoResponse)
-async def update_todo(todo_id: int, request: TodoCreateRequest, db: Session = Depends(get_db)):
-    todo = db.query(Todo).filter(Todo.id == todo_id).first()
-    if not todo:
-        raise HTTPException(status_code=404, detail="Todo not found")
-    for key, value in request.dict().items():
-        setattr(todo, key, value)
-    db.commit()
-    db.refresh(todo)
-    return todo
-
-@app.delete("/todos/{todo_id}")
-async def delete_todo(todo_id: int, db: Session = Depends(get_db)):
-    todo = db.query(Todo).filter(Todo.id == todo_id).first()
-    if not todo:
-        raise HTTPException(status_code=404, detail="Todo not found")
-    db.delete(todo)
-    db.commit()
-    return {"message": "Todo deleted successfully"}
-
-@app.get("/todos/{todo_id}/completed-update", response_model=TodoResponse)
-async def update_todo_completed(todo_id: int, db: Session = Depends(get_db)):
-    todo = db.query(Todo).filter(Todo.id == todo_id).first()
-    if not todo:
-        raise HTTPException(status_code=404, detail="Todo not found")
-    todo.completed = not todo.completed
-    db.commit()
-    db.refresh(todo)
-    return todo
+# Protected API
+@app.get("/protected")
+def protected_route(username: str = Depends(verify_token)):
+    return {"message": f"Hello, {username}. This is a protected route!"}
