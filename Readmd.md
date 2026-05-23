ecbffaac20524d4d8c4c71c01e7c37b029091aef
Eng. Mahmud Ibrahim <mahmud.ibrahim021@gmail.com>
2026-05-24 02:18:04 +0600

    OAuth2+JWT Auth | Secure Routes

--- Readmd.md
+++ Readmd.md
@@ -5,3 +5,5 @@ uvicorn main:app --reload
 
 ## Swagger docs
 http://127.0.0.1:8000/docs
+
+pip install "sqlalchemy" "python-jose" "passlib[bcrypt]" "python-multipart"
--- main.py
+++ main.py
@@ -1,13 +1,38 @@
 from fastapi import FastAPI, HTTPException, Depends, Header
 from jose import jwt, JWTError
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from datetime import datetime,timezone, timedelta
-
+import bcrypt
 
 app = FastAPI()
 
+
+#JWT Configuration
 SECRET_KEY = "mysecretkey"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+#OAuth2 Setup
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+# --- Native Hashing Functions --- 
+def hash_password(password: str):
+    pwd_bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(pwd_bytes, salt).decode('utf-8')
+
+# Verify Password
+def verify_password(plain_password: str, hashed_password: str)-> bool:
+    return bcrypt.checkpw(plain_password.encode('utf-8'), hashed_password.encode('utf-8'))
+
+
+# Dummy User Database
+fake_users_db = {
+    "admin": {
+        "username": "admin",
+        "hashed_password": hash_password("1234"),
+    }
+}
 
 # Create Token
 def create_access_token(data: dict):
@@ -19,7 +44,7 @@ def create_access_token(data: dict):
 
 
 # Verify Token
-def verify_token(token: str = Header(None)):
+def verify_token(token: str = Depends(oauth2_scheme)):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
@@ -29,12 +54,13 @@ def verify_token(token: str = Header(None)):
     except JWTError:
         raise HTTPException(status_code=401, detail="Invalid or Expired token")
 
-# Login API (Token Generate)
+# Login API (Token Generate - OAuth2)
 @app.post("/login")
-def login(username: str, password: str):
-    if username != "admin" or password != "123456":
+def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = fake_users_db.get(form_data.username)
+    if not user or not verify_password(form_data.password, user["hashed_password"]):
         raise HTTPException(status_code=401, detail="Invalid credentials")
-    access_token = create_access_token(data={"sub": username})
+    access_token = create_access_token(data={"sub": form_data.username})
     return {"access_token": access_token, "token_type": "bearer"}
 
 
