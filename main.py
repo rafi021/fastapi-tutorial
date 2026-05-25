92a003470bff9198931cc15d9f6760a79c8e8d0f
Eng. Mahmud Ibrahim <mahmud.ibrahim021@gmail.com>
2026-05-26 02:06:45 +0600

    3rd party API fetch

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
+# import requests
+
+# response = requests.get('https://fakestoreapi.com/products')
+
+# if response.status_code == 200:
+#     print(response.json())
+# else:
+#     print("Failed to retrieve data")
+
+from fastapi import FastAPI
 import requests
 
-response = requests.get('https://fakestoreapi.com/products')
+app = FastAPI()
 
-if response.status_code == 200:
-    print(response.json())
-else:
-    print("Failed to retrieve data")
+# GET ALL Data
+@app.get("/products")
+def get_products():
+    url = 'https://fakestoreapi.com/products'
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return {"error": "Failed to retrieve data"}
